1982cb8ccafcd2f0d4851649096377c3e7dcb3fe
Richard Allison <richard.allison.18@outlook.com>
2018-03-14 20:45:52 +0000

    Added request to itunes charts api and save result to state, and created MusicList.js to contain the chart items

---
+++ src/components/MusicList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MusicList extends Component {
+
+  render() {
+    return (
+      <ul></ul>
+    )
+  }
+
+}
+
+export default MusicList;
--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import MusicList from '../components/MusicList';
 
 class ChartContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartsMusic: []
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+
+    request.addEventListener('load', () => {
+      if (request.status !== 200) return;
+      const jsonString = request.responseText;
+      const chartObjects = JSON.parse(jsonString);
+      this.setState({charts: chartObjects})
+    });
+
+    request.send();
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        <MusicList chartsMusic={this.state.chartsMusic}/>
+      </div>
     )
   }
 }
